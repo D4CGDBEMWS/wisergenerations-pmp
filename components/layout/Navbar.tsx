08472f0575744ff659f6e3f00fdf4d003085f1a6
D4CGDBEMWS <fus.pmp.ce@gmail.com>
2026-04-26 19:16:24 -0400

    Update Calendly link to 30-minute pod session

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { trackEvent } from '@/components/Analytics'
 
-const CALENDLY = 'https://calendly.com/space4grace/15min'
+const CALENDLY = 'https://calendly.com/space4grace/30min-pod'
 
 const navLinks = [
   { label: 'Programs', href: '/programs' },
